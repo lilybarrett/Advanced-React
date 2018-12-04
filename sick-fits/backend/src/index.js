fb29a6faf3dc65af9c92facb1969a8d39891cd6b
lilybarrett <lilydbarrett@gmail.com>
2018-12-03 20:36:41 -0500

    user signup

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,3 +1,4 @@
+const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
 const db = require("./db");
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,4 +1,8 @@
 // this is where our resolvers for our mutations and queries live
+const bcrypt = require("bcryptjs");
+// we will use this to hash our passwords
+const jwt = require("jsonwebtoken");
+
 const mutations = {
     async createItem(parent, args, ctx, info) {
         // TODO: Check if they are logged in
@@ -33,6 +37,31 @@ const mutations = {
         // check if they "own" that item and have permissions to delete it
         // delete it
         return ctx.db.mutation.deleteItem({ where }, info);
+    },
+
+    async signup(parent, args, ctx, info) {
+        args.email = args.email.toLowerCase();
+        const password = await bcrypt.hash(args.password, 10);
+        const user = await ctx.db.mutation.createUser({
+            data: {
+                ...args,
+                password,
+                // this overwrites the args.password into the bcrypted version
+                permissions: { set: ["USER"]}
+                // we do it the above way because we're using an enum and tis is how you set an enum value
+            }
+        }, info);
+
+        // create the JWT token
+        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+        // we set the jwt as a cookie on the response
+        ctx.response.cookie("token", token, {
+            httpOnly: true,
+            // this makes sure we cannot access the token via JavaScript 
+            maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+        });
+        // Finally we return the user to the browser
+        return user;
     }
 };
 
