94b9b4e31bc704d75ec3dbc8c833baedc1177064
Lily Barrett <lily.barrett@ef.com>
2018-12-21 16:42:53 -0500

    WIP permissions management

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require("prisma-binding");
+const { hasPermission } = require("../utils");
 
 const Query = {
     items: forwardTo("db"),
@@ -11,6 +12,16 @@ const Query = {
         return ctx.db.query.user({
             where: { id: ctx.request.userId }
         }, info)
+    },
+    async users(parent, args, ctx, info) {
+        // check if user is logged in
+        if (!ctx.request.userId) {
+            throw new Error('You must be logged in!');
+        }
+        // check if user has permissions to query all users
+        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+        // if they do, query all the users
+        return ctx.db.query.users({}, info);
     }
 };
 
